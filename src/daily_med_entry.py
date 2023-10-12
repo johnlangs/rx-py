edb81dd42ed5ad1807f096e345e56f9b4512512e
John Langs <johnslangs@tamu.edu>
2023-10-11 23:30:42 -0500

    drug entry dataclass

---
+++ src/daily_med_entry.py
@@ -0,0 +1,22 @@
+import os
+import xmltodict
+
+from collections import OrderedDict
+from dataclasses import dataclass
+
+@dataclass
+class daily_med_entry:
+    """
+    Used to store information about one Daily Med Entry.
+    Created after parsing an entry.
+    """
+
+    entry_total_size: bytes # Total size of an entry's data, in bytes. Includes both xml entry and images
+    path: str # path to entry's original folder
+
+    xml_file_path: str
+    parsed_xml_file: OrderedDict[str, Any] # dictonary representation of xml data
+    images: list[str] # list of paths to all images for an ind
+
+    def __init__(self, path: str):
+        self.path = path
