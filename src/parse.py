46aef008b2818b1d738680d81a100acf73ea0fa0
John Langs <johnslangs@tamu.edu>
2023-10-11 23:40:36 -0500

    Updated parsing in-file docs

--- src/parse.py
+++ src/parse.py
@@ -2,7 +2,13 @@ import xmltodict
 import daily_med_entry
 
 def parse_folder():
+    """
+    Loads a Med Daily entry folder into a DailyMedEntry object
+    """
     pass
 
 def parse_dir(path: str) -> list[daily_med_entry.dailyMedEntry]:
+    """
+    Loads an entire folder consisting of Med Daily entry folders
+    """
     pass
