34dbdcff075a2c38448210b0a9ed46a297cf84d5
John Langs <me@johnlangs.net>
2023-12-12 15:15:37 -0600

    dev up

--- src/upload_data.py
+++ src/upload_data.py
@@ -9,14 +9,20 @@ import xmltodict
 """
 Config Vars
 """
+
 data_dir = '../example-data'
 # %%
 """
 Establish client
 """
+
 client = redis.Redis(host="localhost", port=6379, decode_responses=True)
 # %%
-def store_nested_data(redis_client, redis_key, data):
+"""
+
+"""
+
+def find_nested_data(redis_client, redis_key, data):
     """
     Recursively stores nested data in Redis.
     """
@@ -24,15 +30,16 @@ def store_nested_data(redis_client, redis_key, data):
         # If it's a dictionary, iterate through key-value pairs
         for key, value in data.items():
             # Recursively store nested data
-            store_nested_data(redis_client, f"{redis_key}:{key}", value)
+            find_nested_data(redis_client, f"{redis_key}:{key}", value)
     elif isinstance(data, list):
         # If it's a list, iterate through the elements
         for index, element in enumerate(data):
             # Recursively store nested data
-            store_nested_data(redis_client, f"{redis_key}[{index}]", element)
+            find_nested_data(redis_client, f"{redis_key}[{index}]", element)
     else:
         # If it's a leaf node, store the value
-        redis_client.hset(redis_key, "value", json.dumps(data))
+        # redis_client.hset(redis_key, "value", json.dumps(data))
+        print(f"value {str}", json.dumps(data))
 # %%
 """
 Loop through each dir in the data dir folder.
@@ -48,16 +55,9 @@ pp = pprint.PrettyPrinter(indent=4)
 for i, xml_file_path in enumerate(xml_file_paths):
     xml_file = open(xml_file_path)
     dict_file = xmltodict.parse(xml_file.read())
-    print(dict_file)
-    with open('./out.txt', 'w') as file:
-        file.write(pp.pformat(dict_file))
-
+    print(find_nested_data(None, "key", dict_file))
     break
 
     redis_key = f"test_2:{i:03}"
     store_nested_data(client, redis_key, dict_file)
-
-
-
-
 # %%
