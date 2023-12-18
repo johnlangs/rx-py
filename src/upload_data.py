a8911a56ffd209ba5f02ec9d760a8fca0490d505
John Langs <johnslangs@tamu.edu>
2023-12-17 20:18:31 -0600

    dev up

--- src/upload_data.py
+++ src/upload_data.py
@@ -10,13 +10,11 @@ import xml.etree.ElementTree as ET
 """
 Config Vars
 """
-
 data_dir = '../example-data'
 # %%
 """
 Establish client
 """
-
 client = redis.Redis(host="localhost", port=6379, decode_responses=True)
 # %%
 def extract_indications(spl_xml):
@@ -32,8 +30,7 @@ def extract_indications(spl_xml):
         title_text = ''.join(title_element.itertext()).strip()
         return title_text
     else:
-        # If it's a leaf node, store the value
-        redis_client.hset(redis_key, "value", json.dumps(data))
+        return "Title element not found."
 # %%
 """
 Loop through each dir in the data dir folder.
@@ -46,17 +43,8 @@ xml_file_paths = glob.glob(f'{data_dir}/{"*"}/{"*.xml"}')
 # parse all xml files to json
 for i, xml_file_path in enumerate(xml_file_paths):
     xml_file = open(xml_file_path)
-    dict_file = xmltodict.parse(xml_file.read())
-    print(dict_file)
-    with open('./out.txt', 'w') as file:
-        file.write(pp.pformat(dict_file))
-
+    xml_string = xml_file.read()
+    s = extract_indications(xml_string)
+    print(s)
     break
-
-    redis_key = f"test_2:{i:03}"
-    store_nested_data(client, redis_key, dict_file)
-
-
-
-
 # %%
