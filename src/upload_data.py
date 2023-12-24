52aea09e813a63cf436bcb6a09a18d196becacb5
John Langs <me@johnlangs.dev>
2023-12-23 18:57:27 -0600

    dev up

--- src/upload_data.py
+++ src/upload_data.py
@@ -4,7 +4,6 @@ import json
 import os
 import pprint
 import redis
-import xmltodict
 import xml.etree.ElementTree as ET
 # %%
 """
@@ -15,22 +14,39 @@ data_dir = '../example-data'
 """
 Establish client
 """
-client = redis.Redis(host="localhost", port=6379, decode_responses=True)
+# client = redis.Redis(host="localhost", port=6379, decode_responses=True)
 # %%
-def extract_indications(spl_xml):
-    # Parse the XML document
-    tree = ET.fromstring(xml_string)
+def print_all_text(xml_file_path):
+    fields = []
 
-    # Find the title element
-    title_element = tree.find('.//{urn:hl7-org:v3}title')  # Use the namespace in the XPath
+    # Parse the XML file
+    tree = ET.parse(xml_file_path)
+    root = tree.getroot()
 
-    # Check if the title element exists
-    if title_element is not None and title_element:
-        # Accumulate text content from the title element's children
-        title_text = ''.join(title_element.itertext()).strip()
-        return title_text
-    else:
-        return "Title element not found."
+    # Iterate through all text elements in the XML
+    for elem in root.iter():
+        if elem.text and elem.text.strip():
+            print(f"{elem.tag}: {elem.text.strip()}")
+# %%
+def parse_fields(xml_file_path):
+    #FIXME: dummy field for some SPL's that do not label the document's initial title
+    fields = [["dummy"]]
+
+    # Parse the XML file
+    tree = ET.parse(xml_file_path)
+    root = tree.getroot()
+
+    # Iterate through all text elements in the XML
+    for elem in root.iter():
+        if elem.text and elem.text.strip():
+            if "title" in elem.tag:
+                fields.append([elem.text.strip()])
+            elif "item" in elem.tag:
+                fields[len(fields) - 1].append(elem.text)
+            else:
+                fields[len(fields) - 1].append(elem.text.strip())
+
+    return fields
 # %%
 """
 Loop through each dir in the data dir folder.
@@ -38,13 +54,15 @@ Each drug entry dir is expected to follow the DailyMed structure
 with one xml file per drug entry folder. This xml file holds the info
 we will build our embedding based on.
 """
+
 # Grab all files in the data source dir with an xml extension
 xml_file_paths = glob.glob(f'{data_dir}/{"*"}/{"*.xml"}')
+
 # parse all xml files to json
 for i, xml_file_path in enumerate(xml_file_paths):
-    xml_file = open(xml_file_path)
-    xml_string = xml_file.read()
-    s = extract_indications(xml_string)
-    print(s)
-    break
+    fields = parse_fields(xml_file_path)
+    for field in fields:
+        if "USAGE" in field[0]:
+            print(field)
 # %%
+    
